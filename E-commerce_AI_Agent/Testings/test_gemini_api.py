e4c2a3437c52b2e9ed460a3da8c82ea3568d0ab5
PothanSai2004 <136136088+PothanSai2004@users.noreply.github.com>
2025-07-23 10:03:46 +0530

    Delete E-commerce_AI_Agent/Testings directory

--- E-commerce_AI_Agent/Testings/test_gemini_api.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-
-# Paste your API key here
-genai.configure(api_key="AIzaSyBUDPRLWgL_9ydr6ADHofswBn_QBvzi7nw")
-
-model = genai.GenerativeModel('gemini-2.5-pro')
-
-response = model.generate_content("Write a SQL query to get total sales from a table called sales_data")
-print(response.text)
